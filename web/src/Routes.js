887d86d28d01693eca008e811ffc18176c43f74d
Purab Patel <purabtpatel24@gmail.com>
2023-07-16 19:45:44 -0400

    css and background changes

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -13,10 +13,12 @@ import NavigationLayout from './layouts/NavigationLayout/NavigationLayout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/projects" page={ProjectsPage} name="projects" />
-      <Route path="/about" page={AboutPage} name="about" />
+
+
       <Set wrap={NavigationLayout}>
         <Route path="/" page={HomePage} name="home" />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/projects" page={ProjectsPage} name="projects" />
         <Route path="/styles" page={StylesPage} name="styles" />
         <Route path="/stylespage" page={StylesPage} name="stylespage" />
         <Route notfound page={NotFoundPage} />
--- web/src/layouts/NavigationLayout/NavigationLayout.js
+++ web/src/layouts/NavigationLayout/NavigationLayout.js
@@ -2,21 +2,34 @@ import { Link, routes } from '@redwoodjs/router'
 
 const NavigationLayout = ({ children }) => {
   return <>
-    <nav>
-      <ul>
-        <li>
-          <p>purab_patel</p>
-        </li>
-        <li>
-          <Link to={routes.home()} className='link'>_hello</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="header-background">
 
+      <div className="main-background">
+        <nav>
+          <ul>
+            <li>
+              <p className='link'>purab_patel</p>
+            </li>
+            <li>
+              <Link to={routes.home()} className='link'>_hello</Link>
+            </li>
+            <li>
+              <Link to={routes.about()} className='link'>_about</Link>
+            </li>
+            <li>
+              <Link to={routes.projects()} className='link'>_projects</Link>
+            </li>
+          </ul>
+        </nav>
 
-    <main>
-      {children}
-    </main>
+        <div className='header-underline'></div>
+
+        <main>
+          {children}
+        </main>
+        {/* <footer> */}
+      </div>
+    </div>
   </>
 }
 
